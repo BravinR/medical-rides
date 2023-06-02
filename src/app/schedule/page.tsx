d0981fbd4d6b9f5a634828cddd741d014835e2fe
BravinR <bravinrutoh@gmail.com>
2023-06-02 10:14:14 -0600

    Added a schedule page

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Link from 'next/link';
 
 const Schedule = () => {
   return (
@@ -8,14 +9,27 @@ const Schedule = () => {
             <input type="text" id="base-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
         </div>
         <div className="mb-6">
-            <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-black">Address</label>
+            <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-black">Phone Number</label>
             <input type="text" id="base-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
         </div>
         <div className="mb-6">
-            <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-black">Pick up time</label>
+            <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-black">Medicaid ID</label>
+            <input type="text" id="base-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+        </div>
+        <div className="mb-6">
+            <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-black">Pick up time and Date</label>
+            <input type="text" id="base-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+        </div>
+        <div className="mb-6">
+            <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-black">Pick up Address</label>
             <input type="text" id="base-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
         </div>
 
+        <div className="mb-6">
+            <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-black">Drop off Address</label>
+            <input type="text" id="base-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+        </div>
+        <Link href="/" className="focus:outline-none text-black bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 ml-52 mb-2 dark:focus:ring-yellow-900">Submit</Link>
     </div>
   )
 };
