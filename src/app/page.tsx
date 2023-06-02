0d6a8970ee9530ccae1f64ffc01f457c310f49c0
BravinR <bravinrutoh@gmail.com>
2023-06-01 18:50:48 -0600

    Added Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,19 @@
 import Image from 'next/image';
-import logo from "./../../public/logo.png";
+
+import Navbar from '../components/navbar';
+import Footer from '../components/Footer';
 import backgroundImage from "./../../public/dark_colorado.jpg";
 export default function Home() {
   return (
     <div>
-      <nav className="bg-white border-gray-200">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="#" className="flex items-center">
-              <Image className="w-28" src={logo} alt='Peak Elite logo'/>
-          </a>
-          <h1>Call/SMS +1719 233 0707</h1>
-          <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-            <span className="sr-only">Open main menu</span>
-            <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-          </button>
-          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white ">
-              <li>
-                <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 " aria-current="page">Home</a>
-              </li>
-              <li>
-                <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">About</a>
-              </li>
-              <li>
-                <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Services</a>
-              </li>
-              <li>
-                <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Pricing</a>
-              </li>
-              <li>
-                <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Contact</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Navbar/>
       <div className='relative'>
-        <Image className="overflow-hidden" src={backgroundImage} alt='Peak Elite Background Image'/>
-        <h1 className='absolute top-[200px] text-3xl left-5 font-semibold text-white'>Book your ride from anywhere today!</h1>
-        <h1 className="absolute top-[300px] left-5 font-semibold text-white">
+        <Image className="h-[600px] w-full object-cover " src={backgroundImage} alt='Peak Elite Background Image'/>
+        <h1 className='absolute top-[200px] text-4xl text-center font-semibold text-white'>Book your ride from anywhere today!</h1>
+        <h1 className="absolute top-[300px] text-2xl text-center font-semibold text-white">
         Ride with Confidence: Reliable Non-Emergency Medical Transportation Services at Your Fingertips!
         </h1>
-        <button type="button" className=" absolute top-[380px] left-5 focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Book Now</button>
+        <button type="button" className=" absolute top-[450px] focus:outline-none text-black bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 ml-32 mb-2 dark:focus:ring-yellow-900">Schedule a Ride</button>
       </div>
       <div className='ml-10'>
         <h1 className="mt-5 mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">Non Emergency Medicaid Transportation Services</h1>
@@ -145,28 +117,7 @@ export default function Home() {
           </a>
         </div>
       </div>
-      <div>
-        <footer className="bg-white mt-5 shadow dark:bg-gray-800">
-          <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="#" className="hover:underline">Peak Elite™</a>. All Rights Reserved.
-            </span>
-            <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-              <li>
-                <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
-              </li>
-              <li>
-                <a href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
-              </li>
-              <li>
-                <a href="#" className="mr-4 hover:underline md:mr-6">Licensing</a>
-              </li>
-              <li>
-                <a href="#" className="hover:underline">Contact</a>
-              </li>
-            </ul>
-          </div>
-        </footer>
-      </div>
+      <Footer />
     </div>
   )
 }
